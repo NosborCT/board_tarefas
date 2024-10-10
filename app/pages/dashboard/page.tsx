24a183760ca05403da192ad713e32332089587cd
NosborCT <hainiken08@gmail.com>
2024-10-10 17:02:26 -0300

    feat: Criação a validação de usuario para acesso a pagina de dashboard

--- app/pages/dashboard/page.tsx
+++ app/pages/dashboard/page.tsx
@@ -1,11 +1,25 @@
-import Header from "@/app/components/Header";
+
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Meu painel de tarefas",
 };
 
-const Dashboard = () => {
+
+const Dashboard = async () => {
+
+
+  const session = await getServerSession(authOptions);
+
+  if(!session?.user){
+    redirect("/");
+  }
+
+
+
   return (
     <>
     <h1> dashboard</h1>
